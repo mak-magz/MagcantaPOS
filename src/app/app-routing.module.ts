e8ffedaf76dd53ec389420eeabc506fd8a7def70
Mak <marktmagcanta.dev@gmail.com>
2021-03-13 16:32:34 +0800

    remove default folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
     path: 'dashboard',
     loadChildren: () => import('./core/pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   }
