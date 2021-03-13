7d451da969f81becc9b287ef0ba9a834e07e6790
Mak <marktmagcanta.dev@gmail.com>
2021-03-13 16:29:39 +0800

    feat: add dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./core/pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   }
 ];
 
