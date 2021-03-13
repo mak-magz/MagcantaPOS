1108191dacc898ed6bda61dd793fee45c3cb6368
Mak <marktmagcanta.dev@gmail.com>
2021-03-14 00:19:05 +0800

    feat: add database service

---
+++ src/app/core/services/database/database.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+
+import PouchDB from "pouchdb";
+import cordovaSqlitePlugin from "pouchdb-adapter-cordova-sqlite";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DatabaseService {
+
+  constructor() {}
+
+  createDatabase(databaseName: string) {
+    PouchDB.plugin(cordovaSqlitePlugin);
+    return new PouchDB(databaseName);
+  }
+
+  replicateDB(
+      { sourceDB, destinationDB, options }:
+      {
+        sourceDB: PouchDB.Database;
+        destinationDB: string;
+        options: object;
+      }  ) {
+    return sourceDB.replicate.to(destinationDB, options);
+  }
+}
