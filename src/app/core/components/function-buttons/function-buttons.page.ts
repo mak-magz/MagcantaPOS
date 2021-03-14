d2c35397cc90455e3c9908d22761b8860f726e56
Mak <marktmagcanta.dev@gmail.com>
2021-03-14 14:52:35 +0800

    feat: implement add item

--- src/app/core/components/function-buttons/function-buttons.page.ts
+++ src/app/core/components/function-buttons/function-buttons.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { NewItemPage } from 'src/app/shared/components/modals/new-item/new-item.page';
+import { ItemService } from 'src/app/shared/services/inventory/item.service';
 
 @Component({
   selector: 'app-function-buttons',
@@ -10,7 +11,8 @@ import { NewItemPage } from 'src/app/shared/components/modals/new-item/new-item.
 export class FunctionButtonsPage implements OnInit {
 
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private itemSvc: ItemService
   ) { }
 
   ngOnInit() {
@@ -25,6 +27,7 @@ export class FunctionButtonsPage implements OnInit {
 
     if (newItem) {
       console.log(newItem)
+      const { result, error } = await this.itemSvc.addItem({ ...newItem })
     }
   }
 
