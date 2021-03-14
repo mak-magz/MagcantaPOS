abeefe4fd424efe77ee14fe1a1c01cff2989bef1
Mak <marktmagcanta.dev@gmail.com>
2021-03-14 17:26:03 +0800

    refactor: change from one way to two way replication

--- src/app/shared/services/inventory/item.service.ts
+++ src/app/shared/services/inventory/item.service.ts
@@ -10,106 +10,106 @@ import { DatabaseService } from 'src/app/core/services/database/database.service
 
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class ItemService {
-  private readonly dbName: string = 'inventory';
-  private readonly remote: string =
-    'http://couchadmin:couchadmin@127.0.0.1:5984/' + this.dbName;
+	private readonly dbName: string = 'inventory';
+	private readonly remote: string =
+		'http://couchadmin:couchadmin@127.0.0.1:5984/' + this.dbName;
 
-  public itemDB: PouchDB.Database;
+	public itemDB: PouchDB.Database;
 
-  private readonly replicationOptions = {
-    live: true,
-    retry: true,
-  };
+	private readonly replicationOptions = {
+		live: true,
+		retry: true,
+	};
 
-  private items$ = new BehaviorSubject(null);
+	private items$ = new BehaviorSubject(null);
 
-  constructor(private databaseSvc: DatabaseService) {
-    this.itemDB = databaseSvc.createDatabase(this.dbName);
-    this.replicateDB();
-    this.fetchItems();
-    this.listenToChanges();
-  }
+	constructor(private databaseSvc: DatabaseService) {
+		this.itemDB = databaseSvc.createDatabase(this.dbName);
+		this.replicateDB();
+		this.fetchItems();
+		this.listenToChanges();
+	}
 
-  replicateDB() {
-    var rep = this.databaseSvc.replicateDB({
-      sourceDB: this.itemDB,
-      destinationDB: this.remote,
-      options: this.replicationOptions,
-    });
+	replicateDB() {
+		var rep = this.databaseSvc.syncDB({
+			sourceDB: this.itemDB,
+			targetDB: this.remote,
+			options: this.replicationOptions,
+		});
 
-    rep
-      .on('error', (error) => {
-        console.error("Replication error! :", error);
-      });
-  }
+		rep
+			.on('error', (error) => {
+				console.error("Replication error! :", error);
+			});
+	}
 
-  async fetchItems() {
-    try {
-      const result = await this.itemDB.allDocs({
-        include_docs: true,
-        startkey: '_design',
-        descending: true,
-      })
+	async fetchItems() {
+		try {
+			const result = await this.itemDB.allDocs({
+				include_docs: true,
+				startkey: '_design',
+				descending: true,
+			})
 
-      const items = result.rows.map((doc) => doc.doc);
-      this.items$.next([...items])
+			const items = result.rows.map((doc) => doc.doc);
+			this.items$.next([...items])
 
-    } catch (error) {
-      console.error(error)
-    }
-  }
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
-  listenToChanges() {
-    this.itemDB.changes({ since: "now", live: true }).on("change", (data) => {
-      console.log("changed data: ", data);
-      this.fetchItems();
-    });
-  }
+	listenToChanges() {
+		this.itemDB.changes({ since: "now", live: true }).on("change", (data) => {
+			console.log("changed data: ", data);
+			this.fetchItems();
+		});
+	}
 
-  async addItem(itemData: Item) {
-    const timesStamp = new Date().getTime();
-    const id = `${timesStamp}-${Math.floor(Math.random() * 10000)}-${Math.floor(
-      Math.random() * 1000000000
-    )}`;
+	async addItem(itemData: Item) {
+		const timesStamp = new Date().getTime();
+		const id = `${timesStamp}-${Math.floor(Math.random() * 10000)}-${Math.floor(
+			Math.random() * 1000000000
+		)}`;
 
-    try {
-      return {
-        result: await this.itemDB.put({
-          _id: id,
-          ...itemData,
-        }),
-      };
-    } catch (error) {
-      return {
-        error: error,
-      };
-    }
-  }
+		try {
+			return {
+				result: await this.itemDB.put({
+					_id: id,
+					...itemData,
+				}),
+			};
+		} catch (error) {
+			return {
+				error: error,
+			};
+		}
+	}
 
-  async searchItem(barcode: number) {
-    await this.itemDB.createIndex({
-      index: { fields: ["barcode"] },
-    });
+	async searchItem(barcode: number) {
+		await this.itemDB.createIndex({
+			index: { fields: ["barcode"] },
+		});
 
-    try {
-      return {
-        result: await this.itemDB.find({
-          selector: { barcode: barcode },
-        }),
-      };
-    } catch (error) {
-      return {
-        error: error,
-      };
-    }
-  }
+		try {
+			return {
+				result: await this.itemDB.find({
+					selector: { barcode: barcode },
+				}),
+			};
+		} catch (error) {
+			return {
+				error: error,
+			};
+		}
+	}
 
-  /* Getter functions */
+	/* Getter functions */
 
-  get _allItems() {
-    return this.items$.asObservable();
-  }
+	get _allItems() {
+		return this.items$.asObservable();
+	}
 }
