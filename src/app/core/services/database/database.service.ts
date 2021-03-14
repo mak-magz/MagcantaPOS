1a5285ddc65d2a2377d37c039ebc3f1d538fdac1
Mak <marktmagcanta.dev@gmail.com>
2021-03-14 17:25:27 +0800

    feat: add two way replication function

--- src/app/core/services/database/database.service.ts
+++ src/app/core/services/database/database.service.ts
@@ -4,24 +4,28 @@ import PouchDB from "pouchdb";
 import cordovaSqlitePlugin from "pouchdb-adapter-cordova-sqlite";
 
 @Injectable({
-  providedIn: "root",
+	providedIn: "root",
 })
 export class DatabaseService {
 
-  constructor() {}
+	constructor() { }
 
-  createDatabase(databaseName: string) {
-    PouchDB.plugin(cordovaSqlitePlugin);
-    return new PouchDB(databaseName);
-  }
+	createDatabase(databaseName: string) {
+		PouchDB.plugin(cordovaSqlitePlugin);
+		return new PouchDB(databaseName);
+	}
 
-  replicateDB(
-      { sourceDB, destinationDB, options }:
-      {
-        sourceDB: PouchDB.Database;
-        destinationDB: string;
-        options: object;
-      }  ) {
-    return sourceDB.replicate.to(destinationDB, options);
-  }
+	replicateDB(
+		{ sourceDB, destinationDB, options }:
+			{
+				sourceDB: PouchDB.Database;
+				destinationDB: string;
+				options: object;
+			}) {
+		return sourceDB.replicate.to(destinationDB, options);
+	}
+
+	syncDB({ sourceDB, targetDB, options }: { sourceDB: PouchDB.Database, targetDB: string, options: object }) {
+		return sourceDB.sync(targetDB, options);
+	}
 }
