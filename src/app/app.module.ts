cdf3402447e6133ad332868ffeebb84263acfc4b
Mak <marktmagcanta.dev@gmail.com>
2021-03-15 14:56:45 +0800

    build: add angular material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FunctionButtonsPageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FunctionButtonsPageModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
