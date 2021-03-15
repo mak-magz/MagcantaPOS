9a52ca4f691b5918f2ced218c583e3d7657b3981
Mak <marktmagcanta.dev@gmail.com>
2021-03-15 15:38:47 +0800

    feat: add data table

--- src/app/core/pages/inventory/inventory.module.ts
+++ src/app/core/pages/inventory/inventory.module.ts
@@ -8,13 +8,18 @@ import { InventoryPageRoutingModule } from './inventory-routing.module';
 
 import { InventoryPage } from './inventory.page';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    InventoryPageRoutingModule
-  ],
-  declarations: [InventoryPage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		InventoryPageRoutingModule,
+		MatTableModule,
+		MatFormFieldModule
+	],
+	declarations: [InventoryPage]
 })
-export class InventoryPageModule {}
+export class InventoryPageModule { }
--- src/app/core/pages/inventory/inventory.page.ts
+++ src/app/core/pages/inventory/inventory.page.ts
@@ -1,22 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Item } from 'src/app/shared/models/Item';
 import { ItemService } from 'src/app/shared/services/inventory/item.service';
-
+import { map } from 'rxjs/operators'
 @Component({
-  selector: 'app-inventory',
-  templateUrl: './inventory.page.html',
-  styleUrls: ['./inventory.page.scss'],
+	selector: 'app-inventory',
+	templateUrl: './inventory.page.html',
+	styleUrls: ['./inventory.page.scss'],
 })
 export class InventoryPage implements OnInit {
 
-  itemDataSub: Subscription;
-  constructor(private itemSvc: ItemService) { }
+	itemDataSub: Subscription;
+	private dataSource: Observable<Item[]>;
+	private displayedColumns: string[] = [
+		"barcode",
+		"name",
+		"quantity",
+		"unit",
+		"price",
+		"discount",
+		"op",
+	];
 
-  ngOnInit() {
-    this.itemDataSub = this.itemSvc._allItems.subscribe((data) => {
-      if (data) {
-        console.log('item list: ', data);
-      }
-    });
-  }
+	constructor(private itemSvc: ItemService) { }
+
+	ngOnInit() {
+		this.itemDataSub = this.itemSvc._allItems.subscribe((data) => {
+			if (data) {
+				console.log('item list: ', data);
+			}
+		});
+
+		this.dataSource = this.itemSvc._allItems.pipe(map(data => { return data }))
+	}
+
+	/* Getters */
+
+	get _displayedColumns(): String[] {
+		return this.displayedColumns;
+	}
+
+	get _dataSource(): Observable<Item[]> {
+		return this.dataSource;
+	}
 }
