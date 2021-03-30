a46ea0145ff72329c15bf6bbff66eba176823e5a
Mak <marktmagcanta.dev@gmail.com>
2021-03-30 13:50:07 +0800

    feat: add template for all scanned items

--- src/app/core/pages/sales/sales.module.ts
+++ src/app/core/pages/sales/sales.module.ts
@@ -1,3 +1,4 @@
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -9,12 +10,13 @@ import { SalesPageRoutingModule } from './sales-routing.module';
 import { SalesPage } from './sales.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SalesPageRoutingModule
-  ],
-  declarations: [SalesPage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		SalesPageRoutingModule,
+		MatTableModule
+	],
+	declarations: [SalesPage]
 })
-export class SalesPageModule {}
+export class SalesPageModule { }
--- src/app/core/pages/sales/sales.page.ts
+++ src/app/core/pages/sales/sales.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sales',
-  templateUrl: './sales.page.html',
-  styleUrls: ['./sales.page.scss'],
+	selector: 'app-sales',
+	templateUrl: './sales.page.html',
+	styleUrls: ['./sales.page.scss'],
 })
 export class SalesPage implements OnInit {
+	displayedColumns: string[] = [
+		"item",
+		"quantity",
+		"unit",
+		"price",
+		"discount",
+		"total",
+	];
 
-  constructor() { }
+	sales = {
+		subTotal: 0,
+		discount: 0,
+		tax: 0,
+		totalItems: 0,
+		netTotal: 0,
+	};
 
-  ngOnInit() {
-  }
+	displayedFooter: string[] = ["sub", "total"];
+	dataSource: any;
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
